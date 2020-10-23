afd1bd60965778cb65e8df2bc09fb640c1bbbb6c
Kare Morstol <kare@nottoobadsoftware.com>
2020-10-23 22:02:18 +0200

    Skip testing bitbucket.org links as they don't work
    Avoids this error message:
    ```
    - ./_site/blog/iphone-development/a-uipickerview-with-labels/index.html
      *  External link http://bitbucket.org/karemorstol/mysettings/raw/tip/Code/Generic%20classes/LabeledPickerView.h failed: 404 No error
      *  External link http://bitbucket.org/karemorstol/mysettings/raw/tip/Code/Generic%20classes/LabeledPickerView.m failed: 404 No error
    - ./_site/blog/iphone-development/index.html
      *  External link http://bitbucket.org/Scotty/hg-git/overview/ failed: 404 No error
      *  External link http://bitbucket.org/karemorstol/mysettings/wiki/Home failed: 404 No error
    - ./_site/blog/iphone-development/mysettings-a-settings-api-for-the-iphone/index.html
      *  External link http://bitbucket.org/karemorstol/mysettings failed: 404 No error
      *  External link http://bitbucket.org/karemorstol/mysettings/wiki/Installation failed: 404 No error
      *  External link http://bitbucket.org/karemorstol/mysettings/wiki/iPhone.jpg failed: 404 No error
      *  External link http://bitbucket.org/karemorstol/mysettings/wiki/plist.jpg failed: 404 No error
      *  External link http://bitbucket.org/karemorstol/mysettings/wiki/plist_full.jpg failed: 404 No error
    htmlproofer 3.10.2 | Error:  HTML-Proofer found 9 failures!
    ```

--- _test.sh
+++ _test.sh
@@ -1,2 +1,2 @@
 bundle exec jekyll build
-bundle exec htmlproofer --assume-extension --check-html --internal-domains nottoobadsoftware.com  ./_site
+bundle exec htmlproofer --assume-extension --check-html --internal-domains nottoobadsoftware.com --url-ignore "/bitbucket\.org/" ./_site
